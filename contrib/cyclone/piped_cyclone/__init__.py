b722cfd473015251fb52b0d7934c4385b1ec179b
Njal Karevoll <njal@karevoll.no>
2012-03-29 15:41:24 +0200

    moving to 0.4.3-dev

--- contrib/cyclone/piped_cyclone/__init__.py
+++ contrib/cyclone/piped_cyclone/__init__.py
@@ -1,3 +1,3 @@
 # See http://www.python.org/dev/peps/pep-0386/ for version numbering, especially NormalizedVersion
 from distutils import version
-version = version.LooseVersion('0.4.1-dev')
+version = version.LooseVersion('0.4.3-dev')
