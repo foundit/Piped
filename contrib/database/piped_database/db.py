c5f341b4ff0d9bf8aa94b750cae853b32ca7b8b6
Alex Brasetvik <alex@brasetvik.com>
2012-08-27 20:21:40 +0200

    Protect the connection with a lock, and use PipedService's cancellable

--- contrib/database/piped_database/db.py
+++ contrib/database/piped_database/db.py
@@ -1,8 +1,9 @@
 # -*- test-case-name: piped.test.test_database -*-
 
-# Copyright (c) 2010-2011, Found IT A/S and Piped Project Contributors.
+# Copyright (c) 2010-2012, Found IT A/S and Piped Project Contributors.
 # See LICENSE for details.
 import collections
+import copy
 import functools
 import logging
 import heapq
@@ -15,12 +16,13 @@ from twisted.application import service
 from twisted.internet import defer, reactor, threads
 from twisted.python import failure, reflect
 
-from piped import event, exceptions, util
+from piped import event, exceptions, util, service as piped_service
 
 
 logger = logging.getLogger(__name__)
 
 
+# We're extra happy with Postgres-stuff around, but don't require it.
 try:
     import psycopg2
     from txpostgres import txpostgres
@@ -42,6 +44,9 @@ class EngineManager(object, service.Service):
         configuration.setdefault('retry_interval', 5.0)
 
         self._fail_if_configuration_is_invalid(configuration)
+
+        # We'll manipulate the configuration, so copy it.
+        configuration['engine'] = copy.deepcopy(configuration['engine'])
         configuration['engine'].setdefault('proxy', ConnectionProxy(self))
 
         self.is_connected = False
@@ -173,18 +178,41 @@ class ConnectionProxy(sqlalchemy.interfaces.ConnectionProxy):
             raise
 
 
-class PostgresListener(object, service.Service):
+class PostgresListener(piped_service.PipedService):
+    """Service that deals with PostgreSQL NOTIFY-triggers, with
+    support for watching transaction snapshot visibility tresholds.
+
+    This is useful when you need to react quickly to changes in the database.
+
+    Watching transaction id (hence "txid") thresholds can help when
+    implementing sync strategies. By ensuring that notifications of
+    changes are sent in monotonically increasing txid order, we can
+    now that we have everything up to txid if an error occurs, even
+    when transactions commit out of order.
+
+    Note that this approach assumes that you will not have a lot of
+    long-running transactions, as those will temporarily block
+    notifications.
+
+    This service is not intended to be used directly to issue
+    arbitrary SQL. Use the EngineProvider if you need more data from
+    the database on notifications.
+    """
 
     def __init__(self, configuration, profile_name):
+        super(PostgresListener, self).__init__()
         if not txpostgres and psycopg2:
             raise exceptions.ConfigurationError('cannot use PostgresListener without psycopg2 and txpostgres')
 
         self.configuration = configuration
         self.profile_name = profile_name
-        self.currently = util.create_deferred_state_watcher(self)
+        self._lock = defer.DeferredLock()
         self._connection = None
         self._listeners = collections.defaultdict(set)
+
         self._txid_queue = []
+        self._currently_pinging = None
+        self.currently_pinging = util.create_deferred_state_watcher(self, '_currently_pinging')
 
         self.is_connected = False
         self.on_connection_established = event.Event()
@@ -196,7 +224,6 @@ class PostgresListener(object, service.Service):
         configuration.setdefault('txid_poll_interval', .1)      
 
     def _connect(self):
-        # TODO: Failing to connect.
         url = urlparse.urlparse(self.configuration['engine']['url'])
         dsn = dict(
             database=url.path.strip('/'),
@@ -208,38 +235,30 @@ class PostgresListener(object, service.Service):
 
         self._connection = txpostgres.Connection()
         return self._connection.connect(**dsn)
-        
-    def startService(self):
-        if self.running:
-            return
-
-        service.Service.startService(self)
-        self._connect_and_listen()
-
-    def stopService(self):
-        if not self.running:
-            return
-
-        service.Service.stopService(self)
-        if self._connection:
-            self._connection.close()
-        if self._currently:
-            self._currently.cancel()
 
     def _notify(self, notification):
         for queue in self._listeners[notification.channel]:
             queue.put(notification)
 
     @defer.inlineCallbacks
-    def _connect_and_listen(self):
+    def _run_exclusively(self, func, *a, **kw):
+        yield self._lock.acquire()
+        try:
+            result = yield self.cancellable(func(*a, **kw))
+            defer.returnValue(result)
+        finally:
+            self._lock.release()
+
+    @defer.inlineCallbacks
+    def run(self):
         try:
             while self.running:
                 try:
                     logger.info('PostgresListener attempting to connect to "{0}"'.format(self.profile_name))
-                    yield self.currently(self._connect())
+                    yield self._run_exclusively(self._connect)
                     self.is_connected = True
-                    yield self.currently(self._connection.runOperation("SET SESSION TIMEZONE TO 'UTC'"))
-                    yield self.currently(self._connection.runOperation("SET application_name TO '%s-listener'" % self.profile_name))                    
+                    yield self._run_exclusively(self._connection.runOperation, "SET SESSION TIMEZONE TO 'UTC'")
+                    yield self._run_exclusively(self._connection.runOperation, "SET application_name TO '%s-listener'" % self.profile_name)
                     self._connection.addNotifyObserver(self._notify)
                     logger.info('PostgresListener connected "{0}"'.format(self.profile_name))
                     self.on_connection_established(self)
@@ -247,11 +266,11 @@ class PostgresListener(object, service.Service):
                     while self.running:
                         try:
                             if self.is_waiting_for_txid_min:
-                                yield self.currently(util.wait(self.configuration['txid_poll_interval']))
-                                yield self.currently(self._check_txid_threshold())
+                                yield util.wait(self.configuration['txid_poll_interval'])
+                                yield self._check_txid_threshold()
                             else:
-                                yield self.currently(util.wait(self.configuration['ping_interval']))
-                                yield self.currently(self._ping())
+                                yield self.currently_pinging(util.wait(self.configuration['ping_interval']))
+                                yield self.currently_pinging(self._ping())
                         except defer.CancelledError:
                             pass
                     
@@ -269,16 +288,22 @@ class PostgresListener(object, service.Service):
                     pass
 
         except Exception as e:
-            logger.exception('Unhandled exception in _connect_and_listen. Traceback follows')
+            logger.exception('Unhandled exception in PostgresListener.run')
+        finally:
+            if self._connection:
+                self._connection.close()
 
     @defer.inlineCallbacks
     def listen(self, events):
+        """Listens to every named event in *events, and returns a
+        `DeferredQueue` that is populated with notifications.
+        """
         dq = defer.DeferredQueue()
         for event in events:
             if not self._listeners[event]:
                 self._listeners[event].add(dq)
 
-                yield self.currently(self._connection.runOperation('LISTEN "%s"' % event))
+                yield self._run_exclusively(self._connection.runOperation, 'LISTEN "%s"' % event)
                 logger.info('"%s"-listener is now listening to "%s"' % (self.profile_name, event))
             
             self._listeners[event].add(dq)
@@ -287,11 +312,15 @@ class PostgresListener(object, service.Service):
 
     @defer.inlineCallbacks
     def unlisten(self, queue):
+        """ Stops listening for any event that populates *queue*.
+
+        If this results in nothing listening to an event, we'll no longer listen to notifications of that event.
+        """
         for listener_name, queues in self._listeners.items():
             if queue in queues:
                 queues.discard(queue)
                 if not queues:
-                    yield self.currently(self._connection.runOperation('UNLISTEN "%s"' % listener_name))
+                    yield self._run_exclusively(self._connection.runOperation, 'UNLISTEN "%s"' % listener_name)
                     logger.info('"%s"-listener is now NOT listening to "%s"' % (self.profile_name, listener_name))
 
     def wait_for_txid_min(self, txid):
@@ -300,9 +329,10 @@ class PostgresListener(object, service.Service):
         d = defer.Deferred()
         heapq.heappush(self._txid_queue, (txid, d))
 
-        if self._currently:
-            # In case we're waiting to do a ping. Don't wait that long if someone wants txids.
-            self._currently.cancel()
+        if self._currently_pinging:
+            # We don't want to wait a long time when we're waiting for
+            # a txid-min, so cancel any ping-related waiting.
+            self._currently_pinging.cancel()
         return d
 
     @property
@@ -310,11 +340,11 @@ class PostgresListener(object, service.Service):
         return bool(self._txid_queue)
 
     def _ping(self):
-        return self.currently(self._connection.runOperation("SELECT 'ping'"))
+        return self._run_exclusively(self._connection.runOperation, "SELECT 'ping'")
 
     @defer.inlineCallbacks
     def _get_current_txid_min(self):
-        rs = yield self.currently(self._connection.runQuery('SELECT txid_snapshot_xmin(txid_current_snapshot())'))
+        rs = yield self._run_exclusively(self._connection.runQuery, 'SELECT txid_snapshot_xmin(txid_current_snapshot())')
         defer.returnValue(rs[0][0])
 
     @defer.inlineCallbacks
