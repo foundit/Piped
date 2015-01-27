eec8d56c764fb874ad81f438c4b469427bb0489c
Njal Karevoll <njal@karevoll.no>
2015-01-27 13:03:48 +0100

    prepare for next development iteration

--- piped/__init__.py
+++ piped/__init__.py
@@ -1,3 +1,3 @@
 # See http://www.python.org/dev/peps/pep-0386/ for version numbering, especially NormalizedVersion
 from distutils import version
-version = version.LooseVersion('0.5.9')
+version = version.LooseVersion('0.5.10-dev')
