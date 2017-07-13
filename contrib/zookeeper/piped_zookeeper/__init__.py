78cb485772e353622c5b939f4c1560dfe37464f6
Njal Karevoll <njal@karevoll.no>
2017-07-13 22:46:33 +0200

    update to next development iteration version 0.7.2-dev

--- contrib/zookeeper/piped_zookeeper/__init__.py
+++ contrib/zookeeper/piped_zookeeper/__init__.py
@@ -1,3 +1,3 @@
 # See http://www.python.org/dev/peps/pep-0386/ for version numbering, especially NormalizedVersion
 from distutils import version
-version = version.LooseVersion('0.7.1')
+version = version.LooseVersion('0.7.2-dev')
